8b5dce2aaf397ed353704c61837e878099ea6351
koksing456 <koksing456@gmail.com>
2022-03-10 21:43:32 +0800

    Completion of smart contracts and testing

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,13 +3,14 @@ const EthSwap = artifacts.require("EthSwap");
 const Token = artifacts.require("Token");
 
 module.exports = async function(deployer) {
-    //deploy ethswap
-  await deployer.deploy(EthSwap)
-  const ethSwap = await EthSwap.deployed()
 
   //deploy token;
   await deployer.deploy(Token);
-  const token = await Token.deployed()
+  const token = await Token.deployed() 
+
+    //deploy ethswap
+  await deployer.deploy(EthSwap, token.address)
+  const ethSwap = await EthSwap.deployed()
 
   //Transfer all tokens to EthSwap
   await token.transfer(ethSwap.address, '1000000000000000000000000')
--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -2,12 +2,67 @@
 
 pragma solidity ^0.5.0;
 
+//import smart contracts
+import "./Token.sol";
+
 contract EthSwap{
     string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
 
-    function buy(address _from, uint256 _value) private returns(bool success){
+ 
+
+    event TokensPurhased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    event TokensSold(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor(Token _token) public{
+        token = _token;
+    }
+
+    function buyTokens() public payable{
+        //Redemption Rate = value of DApp per eth 
+        //value =  No of eth input * redemption rate
+        uint tokenAmount = msg.value * 100;
+
+        //Make sure that the balance of EthSwap exchange is more than the token amount purchase
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        //msg.sender refers to the person who call this buyTokens function which is the buyer
+        token.transfer(msg.sender, tokenAmount);
+
+        //Emit an event
+        emit TokensPurhased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _amount) public{
+        //Investor cannot sell more token than they have
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        //Calculate the ether amount received
+        uint etherAmount = _amount / rate;
+
+        //Make sure EthSwap has > eth 
+        require(address(this).balance >= etherAmount);
+
+        //Make sure that the balance of account balance is more than the token amount sell
         
-        return true;
+        //send etherAmount to the msg.sender who call this function
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        //Emit an event
+        emit TokensSold(address(this), address(token), _amount, rate);
     }
 }
 
