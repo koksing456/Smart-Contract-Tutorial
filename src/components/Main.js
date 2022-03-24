3f2e7fa7c8dbbf1349452ec1ecc3947169569221
koksing456 <koksing456@gmail.com>
2022-03-25 02:40:53 +0800

    Frontend using JS
    To send and retrieve data from the smart contract

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+//Client Application
+class Main extends Component {
+
+  render() {
+    return (
+      <div>
+          <h1>Hello blockchain</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
